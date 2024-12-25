4212bc9feebd7edc1308e91dc8b85de8af7003df
skarokin <sean.kim0327@gmail.com>
2024-12-25 01:22:22 -0500

    small optimization to ground truth creation

--- data/create_dataset.py
+++ data/create_dataset.py
@@ -8,26 +8,24 @@ def create_ground_truth():
     data = pd.read_csv(csv_path)
     data.sort_values(by=['seasonYear', 'weekNumber'], inplace=True)
 
-    # add unique identifier column (for use in creating ground truth only), not used in calculating nextWeekRank
-    data['uniqueId'] = data['seasonYear'].astype(str) + '_' + data['weekNumber'].astype(str) + '_' + data['teamName']
-
     # create a key for current week
-    data['currentWeekKey'] = data['teamName'] + '_' + data['seasonYear'].astype(str) + '_' + data['weekNumber'].astype(str)
+    data['currentWeekId'] = data['teamName'] + '_' + data['seasonYear'].astype(str) + '_' + (data['weekNumber']).astype(str)
     
     # create a key for next week
-    data['nextWeekKey'] = data['teamName'] + '_' + data['seasonYear'].astype(str) + '_' + (data['weekNumber'] + 1).astype(str)
+    data['nextWeekId'] = data['teamName'] + '_' + data['seasonYear'].astype(str) + '_' + (data['weekNumber'] + 1).astype(str)
 
-    # map each currentWeekKey to its corresponding apRank
-    all_week_ranks = data.set_index('currentWeekKey')['apRank'].to_dict()
+    # this just creates a dictionary that maps currentWeekId to apRank
+    # the nature of this means next week's game can simply be looked up with the nextWeekId
+    all_week_ranks = data.set_index('currentWeekId')['apRank'].to_dict()
 
     # since all_week_ranks maps ALL weeks to their apRank, we can then
     # populate nextWeekRank by simply looking up team X's next week game (this is the purpose of unique identifier for
     # next week's game) and looking up the apRank for that week
     # even if the next week's game is not in the data (for example end of season), the nextWeekRank will be NaN
-    data['nextWeekRank'] = data['nextWeekKey'].map(all_week_ranks)
+    data['nextWeekRank'] = data['nextWeekId'].map(all_week_ranks)
 
     # create ground truth
-    ground_truth = data[['uniqueId', 'nextWeekRank']]
+    ground_truth = data[['currentWeekId', 'nextWeekRank']]
 
     return ground_truth
 
